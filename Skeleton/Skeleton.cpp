540b5c8ef1d6a79e54b6af94680e52221e8663d2
HLCaptain <pkblazsak@gmail.com>
2021-04-15 17:42:46 +0200

    quaternion rotaion doesnt work as intended, but made an interesting scene

--- Skeleton/Skeleton.cpp
+++ Skeleton/Skeleton.cpp
@@ -67,125 +67,19 @@ const char* fragmentSource = R"(
 	#version 450
 	precision highp float;
 
-	struct Material {
-		vec3 ka, kd, ks;
-		float  shininess;
-		vec3 F0;
-		int rough, reflective;
-	};
-
-	struct Light {
-		vec3 direction;
-		vec3 Le, La;
-	};
-
-	struct Sphere {
-		vec3 center;
-		float radius;
-	};
-
-	struct Hit {
-		float t;
-		vec3 position, normal;
-		int mat;	// material index
-	};
-
-	struct Ray {
-		vec3 start, dir;
-	};
-
-	const int nMaxObjects = 500;
-
-	uniform vec3 wEye; 
-	uniform Light light;     
-	uniform Material materials[2];  // diffuse, specular, ambient ref
-	uniform int nObjects;
-	uniform Sphere objects[nMaxObjects];
-
 	in  vec3 p;					// point on camera window corresponding to the pixel
 	out vec4 fragmentColor;		// output that goes to the raster memory as told by glBindFragDataLocation
 
-	Hit intersect(const Sphere object, const Ray ray) {
-		Hit hit;
-		hit.t = -1;
-		vec3 dist = ray.start - object.center;
-		float a = dot(ray.dir, ray.dir);
-		float b = dot(dist, ray.dir) * 2.0f;
-		float c = dot(dist, dist) - object.radius * object.radius;
-		float discr = b * b - 4.0f * a * c;
-		if (discr < 0) return hit;
-		float sqrt_discr = sqrt(discr);
-		float t1 = (-b + sqrt_discr) / 2.0f / a;	// t1 >= t2 for sure
-		float t2 = (-b - sqrt_discr) / 2.0f / a;
-		if (t1 <= 0) return hit;
-		hit.t = (t2 > 0) ? t2 : t1;
-		hit.position = ray.start + ray.dir * hit.t;
-		hit.normal = (hit.position - object.center) / object.radius;
-		return hit;
-	}
-
-	Hit firstIntersect(Ray ray) {
-		Hit bestHit;
-		bestHit.t = -1;
-		for (int o = 0; o < nObjects; o++) {
-			Hit hit = intersect(objects[o], ray); //  hit.t < 0 if no intersection
-			if (o < nObjects/2) hit.mat = 0;	 // half of the objects are rough
-			else			    hit.mat = 1;     // half of the objects are reflective
-			if (hit.t > 0 && (bestHit.t < 0 || hit.t < bestHit.t))  bestHit = hit;
-		}
-		if (dot(ray.dir, bestHit.normal) > 0) bestHit.normal = bestHit.normal * (-1);
-		return bestHit;
-	}
-
-	bool shadowIntersect(Ray ray) {	// for directional lights
-		for (int o = 0; o < nObjects; o++) if (intersect(objects[o], ray).t > 0) return true; //  hit.t < 0 if no intersection
-		return false;
-	}
-
-	vec3 Fresnel(vec3 F0, float cosTheta) { 
-		return F0 + (vec3(1, 1, 1) - F0) * pow(cosTheta, 5);
-	}
-
-	const float epsilon = 0.0001f;
-	const int maxdepth = 5;
-
-	vec3 trace(Ray ray) {
-		vec3 weight = vec3(1, 1, 1);
-		vec3 outRadiance = vec3(0, 0, 0);
-		for(int d = 0; d < maxdepth; d++) {
-			Hit hit = firstIntersect(ray);
-			if (hit.t < 0) return weight * light.La;
-			if (materials[hit.mat].rough == 1) {
-				outRadiance += weight * materials[hit.mat].ka * light.La;
-				Ray shadowRay;
-				shadowRay.start = hit.position + hit.normal * epsilon;
-				shadowRay.dir = light.direction;
-				float cosTheta = dot(hit.normal, light.direction);
-				if (cosTheta > 0 && !shadowIntersect(shadowRay)) {
-					outRadiance += weight * light.Le * materials[hit.mat].kd * cosTheta;
-					vec3 halfway = normalize(-ray.dir + light.direction);
-					float cosDelta = dot(hit.normal, halfway);
-					if (cosDelta > 0) outRadiance += weight * light.Le * materials[hit.mat].ks * pow(cosDelta, materials[hit.mat].shininess);
-				}
-			}
-
-			if (materials[hit.mat].reflective == 1) {
-				weight *= Fresnel(materials[hit.mat].F0, dot(-ray.dir, hit.normal));
-				ray.start = hit.position + hit.normal * epsilon;
-				ray.dir = reflect(ray.dir, hit.normal);
-			} else return outRadiance;
-		}
-	}
+	// COPY FRAGMENT SHADER HERE FOR FINAL FILE
+	// COPY FRAGMENT SHADER HERE FOR FINAL FILE
+	// COPY FRAGMENT SHADER HERE FOR FINAL FILE
 
 	void main() {
-		Ray ray;
-		ray.start = wEye; 
-		ray.dir = normalize(p - wEye);
-		fragmentColor = vec4(trace(ray), 1); 
+		fragmentColor = vec4(abs(p.x), abs(p.y), 1, 1); 
 	}
 )";
 
-enum MaterialType { ROUGH, REFLECTIVE, SMOOTH };
+enum MaterialType { ROUGH, REFLECTIVE };
 
 //---------------------------
 struct Material {
@@ -201,7 +95,7 @@ struct Material {
 //---------------------------
 struct RoughMaterial : Material {
 //---------------------------
-	RoughMaterial(vec3 _kd, vec3 _ks, float _shininess) : Material(ROUGH) {
+	RoughMaterial(vec3 _kd = vec3(0.0f, 0.0f, 0.0f), vec3 _ks = vec3(0.0f, 0.0f, 0.0f), float _shininess = 10) : Material(ROUGH) {
 		ka = _kd * M_PI; // ambient
 		kd = _kd; // diffuse
 		ks = _ks; // specular
@@ -211,22 +105,16 @@ struct RoughMaterial : Material {
 	}
 };
 
-//---------------------------
-struct SmoothMaterial : Material {
-//---------------------------
-	SmoothMaterial(vec3 _F0) : Material(SMOOTH) {
-		F0 = _F0;
-		rough = false;
-		reflective = true;
-	}
-};
 const vec3 one(1, 1, 1);
 vec3& operator/(const vec3& num, const vec3& denom) {
 	return vec3(num.x / denom.x, num.y / denom.y, num.z / denom.z);
 }
+
 struct ReflectiveMaterial : Material {
-	ReflectiveMaterial(const vec3& n, const vec3& kappa) : Material(REFLECTIVE) {
-		F0 = ((n - one) * (n - one) + kappa * kappa) / ((n + one) + (n + one) + kappa * kappa);
+	ReflectiveMaterial(const vec3& n = vec3(1.0f, 1.0f, 1.0f), const vec3& kappa = vec3(1.0f, 1.0f, 1.0f)) : Material(REFLECTIVE) {
+		F0 = ((n - one) * (n - one) + kappa * kappa) / ((n + one) * (n + one) + kappa * kappa);
+		rough = false;
+		reflective = true;
 	}
 };
 
@@ -235,8 +123,31 @@ struct Sphere {
 //---------------------------
 	vec3 center;
 	float radius;
+	Sphere(const vec3& _center = vec3(0.0f, 0.0f, 0.0f), float _radius = 1.0f) :
+		center(_center),
+		radius(_radius) { }
+};
 
-	Sphere(const vec3& _center, float _radius) { center = _center; radius = _radius; }
+//---------------------------
+struct Quadratic {
+//---------------------------
+	vec3 center;
+	float a, b, c;
+	Quadratic(const vec3& _center = vec3(0.0f, 0.0f, 0.0f), const float& _a = 1.0f, const float& _b = 1.0f, const float& _c = 1.0f) :
+		center(_center),
+		a(_a),
+		b(_b),
+		c(_c) { }
+};
+
+//---------------------------
+struct Dodecahedron {
+	//---------------------------
+	std::vector<vec3> vertices;
+	std::vector<int> faces;
+	Dodecahedron(const std::vector<vec3>& vertices = std::vector<vec3>(), const std::vector<int>& planes = std::vector<int>()) :
+		vertices(vertices),
+		faces(planes) { }
 };
 
 long curTime = 0;
@@ -246,6 +157,7 @@ struct Camera {
 //---------------------------
 	vec3 eye, lookat, right, up; // lookat would have been nice to name it "target" smh
 	float fov;
+	float speed;
 public:
 	void set(vec3 _eye, vec3 _lookat, vec3 vup, float _fov) {
 		eye = _eye;
@@ -257,15 +169,23 @@ public:
 		up = normalize(cross(w, right)) * f * tanf(fov / 2);
 	}
 	void Animate(float dt) {
-		eye = vec3((eye.x - lookat.x) * cos(dt / 10) + (eye.z - lookat.z) * sin(dt / 10) + lookat.x,
+		eye = vec3((eye.x - lookat.x) * cos(dt * speed) + (eye.z - lookat.z) * sin(dt * speed) + lookat.x,
 			eye.y /*sin(curTime / 200.0f) / 20 + 45 * M_PI / 180*/,
-			-(eye.x - lookat.x) * sin(dt / 10) + (eye.z - lookat.z) * cos(dt / 10) + lookat.z); // rotating around target
+			-(eye.x - lookat.x) * sin(dt * speed) + (eye.z - lookat.z) * cos(dt * speed) + lookat.z); // rotating around target
+		// //eye = vec3((eye.x - lookat.x) * cos(dt * speed) + (eye.z - lookat.z) * sin(dt * speed) + lookat.x,
+		//	eye.y /*sin(curTime / 200.0f) / 20 + 45 * M_PI / 180*/,
+		//	-(eye.x - lookat.x) * sin(dt * speed) + (eye.z - lookat.z) * cos(dt * speed) + lookat.z); // rotating around target
 		//fov = sin(curTime / 200.0f) / 20 + 45 * M_PI / 180;
 		set(eye, lookat, up, fov);
 	}
+	Camera() : speed(1.0f) { }
 };
 
-enum LightType { POINTLIGHT, DIRECTIONAL };
+float Fresnel(const float& n, const float& kappa) {
+	return ((n - 1) * (n - 1) + kappa * kappa) / ((n + 1) * (n + 1) + kappa * kappa);
+}
+
+enum LightType { POINTLIGHT, DIRECTIONAL, LIGHT };
 
 //---------------------------
 struct Light {
@@ -276,7 +196,7 @@ struct Light {
 	virtual vec3 getDir() = 0;
 	virtual vec3 getPos() { return position; }
 protected:
-	Light(LightType _type, vec3 _Le, vec3 _La) :
+	Light(LightType _type = LIGHT, vec3 _Le = vec3(0.0f, 0.0f, 0.0f), vec3 _La = vec3(0.0f, 0.0f, 0.0f)) :
 		Le(_Le),
 		La(_La),
 		position(vec3(0, 0, 0)),
@@ -284,16 +204,17 @@ protected:
 };
 
 struct PointLight : public Light {
-	PointLight(vec3 _position, vec3 _Le, vec3 _La) : 
+	PointLight(vec3 _position = vec3(0.0f, 0.0f, 0.0f), vec3 _Le = vec3(0.0f, 0.0f, 0.0f), vec3 _La = vec3(0.0f, 0.0f, 0.0f)) :
 		Light(POINTLIGHT, _Le, _La) {
 		position = _position;
 	}
 	vec3 getDir() { return -1 * position; }
+	vec3 getPos() { return position; }
 };
 
 struct DirectionalLight : public Light {
 	vec3 direction;
-	DirectionalLight(vec3 _direction, vec3 _Le, vec3 _La) :
+	DirectionalLight(vec3 _direction = vec3(0.0f, 0.0f, 0.0f), vec3 _Le = vec3(0.0f, 0.0f, 0.0f), vec3 _La = vec3(0.0f, 0.0f, 0.0f)) :
 		Light(DIRECTIONAL, _Le, _La),
 		direction(normalize(_direction)) { }
 	vec3 getDir() { return direction; }
@@ -303,24 +224,38 @@ struct DirectionalLight : public Light {
 class Shader : public GPUProgram {
 //---------------------------
 public:
-	void setUniformMaterials(const std::vector<Material*>& materials) {
+	void setUniformMaterials(const Material& dodDifMat, const Material& dodRefMat, const Material& chipsMat) {
 		char name[256];
-		for (unsigned int mat = 0; mat < materials.size(); mat++) {
-			sprintf(name, "materials[%d].ka", mat); setUniform(materials[mat]->ka, name);
-			sprintf(name, "materials[%d].kd", mat); setUniform(materials[mat]->kd, name);
-			sprintf(name, "materials[%d].ks", mat); setUniform(materials[mat]->ks, name);
-			sprintf(name, "materials[%d].shininess", mat); setUniform(materials[mat]->shininess, name);
-			sprintf(name, "materials[%d].F0", mat); setUniform(materials[mat]->F0, name);
-			sprintf(name, "materials[%d].rough", mat); setUniform(materials[mat]->rough, name);
-			sprintf(name, "materials[%d].reflective", mat); setUniform(materials[mat]->reflective, name);
-		}
+		// diffuse walls of dod
+		sprintf(name, "materials[%d].ka", 0);  setUniform(dodDifMat.ka, name);
+		sprintf(name, "materials[%d].kd", 0);  setUniform(dodDifMat.kd, name);
+		sprintf(name, "materials[%d].ks", 0);  setUniform(dodDifMat.ks, name);
+		sprintf(name, "materials[%d].shininess", 0);  setUniform(dodDifMat.shininess, name);
+		sprintf(name, "materials[%d].F0", 0);  setUniform(dodDifMat.F0, name);
+		sprintf(name, "materials[%d].rough", 0);  setUniform(dodDifMat.rough, name);
+		sprintf(name, "materials[%d].reflective", 0);  setUniform(dodDifMat.reflective, name);
+		// reflective portals of dod
+		sprintf(name, "materials[%d].ka", 1);  setUniform(dodRefMat.ka, name);
+		sprintf(name, "materials[%d].kd", 1);  setUniform(dodRefMat.kd, name);
+		sprintf(name, "materials[%d].ks", 1);  setUniform(dodRefMat.ks, name);
+		sprintf(name, "materials[%d].shininess", 1);  setUniform(dodRefMat.shininess, name);
+		sprintf(name, "materials[%d].F0", 1);  setUniform(dodRefMat.F0, name);
+		sprintf(name, "materials[%d].rough", 1);  setUniform(dodRefMat.rough, name);
+		sprintf(name, "materials[%d].reflective", 1);  setUniform(dodRefMat.reflective, name);
+		// reflective material of quad chips
+		sprintf(name, "materials[%d].ka", 2);  setUniform(chipsMat.ka, name);
+		sprintf(name, "materials[%d].kd", 2);  setUniform(chipsMat.kd, name);
+		sprintf(name, "materials[%d].ks", 2);  setUniform(chipsMat.ks, name);
+		sprintf(name, "materials[%d].shininess", 2);  setUniform(chipsMat.shininess, name);
+		sprintf(name, "materials[%d].F0", 2);  setUniform(chipsMat.F0, name);
+		sprintf(name, "materials[%d].rough", 2);  setUniform(chipsMat.rough, name);
+		sprintf(name, "materials[%d].reflective", 2);  setUniform(chipsMat.reflective, name);
 	}
 
-	void setUniformLight(Light* light) {
-		setUniform(light->La, "light.La");
-		setUniform(light->Le, "light.Le");
-		//setUniform(light->getDir(), "light.direction");
-		setUniform(light->getPos(), "light.position");
+	void setUniformLight(const PointLight& light) {
+		setUniform(light.La, "light.La");
+		setUniform(light.Le, "light.Le");
+		setUniform(light.position, "light.position");
 	}
 
 	void setUniformCamera(const Camera& camera) {
@@ -330,16 +265,31 @@ public:
 		setUniform(camera.up, "wUp");
 	}
 
-	void setUniformObjects(const std::vector<Sphere*>& objects) {
-		setUniform((int)objects.size(), "nObjects");
+	void setUniformObjects(const Quadratic& quad, const Dodecahedron& dod) {
 		char name[256];
-		for (unsigned int o = 0; o < objects.size(); o++) {
-			sprintf(name, "objects[%d].center", o);  setUniform(objects[o]->center, name);
-			sprintf(name, "objects[%d].radius", o);  setUniform(objects[o]->radius, name);
+		for (unsigned int i = 0; i < dod.faces.size(); i++) {
+			sprintf(name, "dod.faces[%d]", i);  setUniform(dod.faces[i], name);
 		}
+		for (unsigned int i = 0; i < dod.vertices.size(); i++) {
+			sprintf(name, "dod.vertices[%d]", i);  setUniform(dod.vertices[i], name);
+		}
+
+		// maybe useless copying abc
+		setUniform(quad.a, "quad.a");
+		setUniform(quad.b, "quad.b");
+		setUniform(quad.c, "quad.c");
+
+		// uploading the encasing of the quad
+		setUniform(vec3(0.0f, 0.0f, 0.0f), "quadSphere.center");
+		setUniform(0.3f, "quadSphere.radius");
+	}
+
+	void setUniformTime(const long& time) {
+		setUniform((float)time, "timeMs");
 	}
 };
 
+/// Random float number between 0...1
 float rnd() { return (float)rand() / RAND_MAX; }
 
 Shader shader; // vertex and fragment shaders
@@ -347,17 +297,20 @@ Shader shader; // vertex and fragment shaders
 //---------------------------
 class Scene {
 //---------------------------
-	std::vector<Sphere*> objects;
-	std::vector<Light*> lights;
 	Camera camera;
-	std::vector<Material*> materials;
-	std::vector<vec3> dodecahedronVertices;
-	std::vector<int> dodecahedronPlanes;
+	RoughMaterial dodDifMat;
+	ReflectiveMaterial dodRefMat;
+	ReflectiveMaterial chipsMat;
+	Dodecahedron dod;
+	Quadratic quad;
+	PointLight pointLight;
 public:
 	void build() {
+
+		// INI DODECAHEDRON
 		const float g = 0.618f;
 		const float G = 1.618f;
-		dodecahedronVertices = std::vector<vec3>({
+		std::vector<vec3> dodecahedronVertices = std::vector<vec3>({
 			vec3(0, g, G),
 			vec3(0, -g, G),
 			vec3(0, -g, -G),
@@ -379,10 +332,10 @@ public:
 			vec3(-1, 1, -1),
 			vec3(-1, -1, -1),
 			});
-
-		dodecahedronPlanes = std::vector<int>({
+		std::vector<int> dodecahedronPlanes = std::vector<int>({
 			1,2,16,5,13, // f1
-			1,13,9,10,14, // f3
+			1,13,9,10,14, // f2
+			1,14,6,15,2, // f3
 			2,15,11,12,16, // f4
 			3,4,18,8,17, // f5
 			3,17,12,11,20, // f6
@@ -393,45 +346,52 @@ public:
 			14,10,19,7,6, // f11
 			6,7,20,11,15 // f12
 			});
+		dod.faces = dodecahedronPlanes;
+		dod.vertices = dodecahedronVertices;
+		// INI DODECAHEDRON END
 
-		vec3 eye = vec3(0, 0, 2);
+		// INI CAMERA
+		vec3 eye = vec3(0, 0, 1);
 		vec3 vup = vec3(0, 1, 0);
 		vec3 lookat = vec3(0, 0, 0);
-		float fov = 45 * (float)M_PI / 180;
+		float fov = 90 * (float)M_PI / 180;
 		camera.set(eye, lookat, vup, fov);
+		camera.speed = 0.4f;
+		// INI CAMERA END
 
-		//lights.push_back(new DirectionalLight(vec3(1, 1, 1), vec3(3, 3, 3), vec3(0.4f, 0.3f, 0.3f)));
-		lights.push_back(new PointLight(vec3(0, 0, 0), vec3(2, 2, 2), vec3(0.4f, 0.3f, 0.3f)));
+		// INI LIGHT
+		pointLight = PointLight(vec3(0.0f, 0.0f, 0.0f), vec3(1.0f, 1.0f, 1.0f), vec3(0.4f, 0.3f, 0.3f));
+		// INI LIGHT END
 
+		// INI MATERIALS
+		// honey: vec3(1.0f, 0.4f, 0.10f), purple: vec3(6.0f, 0.1f, 0.9f), testgrey: vec3(0.4f, 0.4f, 0.4f)
 		// Az arany törésmutatója és kioltási tényezõje: n/k: 0.17/3.1, 0.35/2.7, 1.5/1.9
-		vec3 kdGold(0.17f, 0.35f, 1.5f), ksGold(3.1, 2.7, 1.9);
-		vec3 kd(0.3f, 0.2f, 0.1f), ks(10, 10, 10);
-		//materials.push_back(new RoughMaterial(kd, ks, 50));
-		materials.push_back(new RoughMaterial(kdGold, ksGold, 150));
-		//materials.push_back(new SmoothMaterial(vec3(0.9f, 0.85f, 0.8f)));
-		materials.push_back(new SmoothMaterial(vec3(kdGold.y/ksGold.y, kdGold.z/ksGold.z, kdGold.x/kdGold.x)));
+		vec3 nGold(0.17f, 0.35f, 1.5f), kappaGold(3.1f, 2.7f, 1.9f);
+		chipsMat = ReflectiveMaterial(nGold, kappaGold);
+		dodDifMat = RoughMaterial(vec3(1.0f, 0.5f, 0.15f), vec3(1.0f, 1.0f, 1.0f), 10.0f); // (kd) diffuse: RGB, (ks) specular: RGB
+		dodRefMat = ReflectiveMaterial(vec3(0.1f, 0.1f, 0.1f), vec3(1.0f, 1.0f, 1.0f));
+		// INI MATERIALS END
 
-		objects.push_back(new Sphere(vec3(0.2f, 0.2f, 0.2f), 0.04f)); // we use the first sphere
-		for (int i = 0; i < 20; i++)
-			objects.push_back(new Sphere(vec3(rnd() - 0.5f, rnd() - 0.5f, rnd() - 0.5f), fmaxf(rnd() * 0.1f, 0.04f)));
-		
+		// INI QUAD
+		// quadratic equation: exp(something) - 1 = 0
+		// quadratic equation simplified: ax + by - cz = 0
+		quad = Quadratic(vec3(0.2f, 0.2f, 0.2f), 3.2f, 4.2f, 2.8f); // TODO: MODIFY a,b,c
+		// INI QUAD END
 	}
 
 	void setUniform(Shader& shader) {
-		shader.setUniformObjects(objects);
-		shader.setUniformMaterials(materials);
-		shader.setUniformLight(lights[0]);
+		shader.setUniformObjects(quad, dod);
+		shader.setUniformMaterials(dodDifMat, dodRefMat, chipsMat);
+		shader.setUniformLight(pointLight);
 		shader.setUniformCamera(camera);
+		shader.setUniformTime(curTime);
 	}
 
 	void Animate(float dt) { 
 		camera.Animate(dt);
-		// rotating around target
-		objects[0]->center.x = ((objects[0]->center.x - camera.lookat.x) * cos(dt) + (objects[0]->center.z - camera.lookat.z) * sin(dt) + camera.lookat.x) / 5.0f;
-		objects[0]->center.z = (-(objects[0]->center.x - camera.lookat.x) * sin(dt) + (objects[0]->center.z - camera.lookat.z) * cos(dt) + camera.lookat.z) / 5.0f;
-		objects[0]->radius += dt / 100.0f;
-		shader.setUniformObjects(objects);
 	}
+
+	Scene() { }
 };
 
 Scene scene;
